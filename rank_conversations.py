c1c8021b63c23dcf22d2cb9414754468aa0f9683
xing-yu <staryu.udi@gmail.com>
2018-10-17 16:07:08 -0400

    add a script to rank conversations by word count

---
+++ rank_conversations.py
@@ -0,0 +1,88 @@
+# this program ranks conversations by word count
+
+import sys
+
+if len(argv) < 2:
+	print("usage: script input_file")
+	sys.exit(-1)
+
+fname = sys.argv[1]	# input file name, format is conversation
+
+#------------------- main function --------------------------
+def main(fname):
+
+	fin = open(fname)
+
+	conversations = parse_conversations(fin)
+
+	conversations_sorted = sort_conversations(conversations)
+
+	fout = open("annotation_sort.txt", 'w')
+
+	for each in conversations_sorted:
+
+		fout.write(each[1])
+
+	fin.close()
+	fout.close()
+
+#----------------- parse conversations ----------------------
+def parse_conversations(f):
+
+	ret = []
+
+	mark = False # mark body
+
+	for line in f:
+
+		# end of a conversation
+
+		if line == "==========":
+
+			word_count = len(body.split(' '))
+
+			ret.append((word_count, content))
+
+			content = ''
+			body = ''
+
+		else:
+
+			content += line
+
+			if line == "COMMENT":
+				mark = True
+
+			if line == "END_OF COMMENT":
+				mark = False
+
+			if mark == True:
+				body += line
+
+	return ret
+
+#----------------- rank conversation ------------------------
+# convers: [(int, str)]
+
+def sort_conversations(convers):
+
+	from functools import cmp_to_key
+
+	ret = sorted(convers, key = cmp_to_key(cmp))
+
+	return ret
+
+#------------------ cmp function -----------------------------
+
+def cmp(a, b):
+
+	if a[0] < b[0]:
+
+		return -1
+
+	else:
+
+		return 1
+
+#------------------ run main ---------------------------------
+main(fname)
